7bea281d09e9c46a6a30b4b83d2f7959246f5e23
catacornejol <catalina.cornejo01@gmail.com>
2026-02-22 19:04:44 -0300

    Change app to use dynamic port from environment variable
    Updated the app to use a dynamic port from environment variables.

--- app.py
+++ app.py
@@ -52,5 +52,5 @@ Llegada: {llegada}
         return f"Error al enviar: {e}"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
